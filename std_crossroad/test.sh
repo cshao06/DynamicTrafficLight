e7c657e5fef38ec470c94ca82772f03bebd2d14a
SiyuanZheng <56163403+SiyuanZheng@users.noreply.github.com>
2019-12-09 19:09:27 -0500

    test.sh

--- std_crossroad/test.sh
+++ std_crossroad/test.sh
@@ -2,4 +2,39 @@ python runnerTemp.py --nogui --veh-period 2 --ped-period 18 -o output/v2p18.xml 
 python runnerTemp.py --nogui --veh-period 3 --ped-period 4.5 -o output/v3p4_5.xml &&
 python runnerTemp.py --nogui --veh-period 3.6 --ped-period 3.6 -o output/v3_6p3_6.xml &&
 python runnerTemp.py --nogui --veh-period 4.5 --ped-period 3 -o output/v4_5p3.xml &&
-python runnerTemp.py --nogui --veh-period 18 --ped-period 2 -o output/v18p2.xml
+python runnerTemp.py --nogui --veh-period 18 --ped-period 2 -o output/v18p2.xml &&
+python runnerTemp.py --nogui -e 30 -o output/e30.xml &&
+python runnerTemp.py --nogui -e 60 -o output/e60.xml &&
+python runnerTemp.py --nogui -e 90 -o output/e90.xml &&
+python runnerTemp.py --nogui -e 120 -o output/e120.xml &&
+python runnerTemp.py --nogui -e 150 -o output/e150.xml &&
+python runnerTemp.py --nogui --veh-period 1 --ped-period 1 -o output/v1p1.xml &&
+python runnerTemp.py --nogui --veh-period 2 --ped-period 2 -o output/v2p2.xml &&
+python runnerTemp.py --nogui --veh-period 3 --ped-period 3 -o output/v3p3.xml &&
+python runnerTemp.py --nogui --veh-period 4 --ped-period 4 -o output/v4p4.xml &&
+python runnerTemp.py --nogui --veh-period 5 --ped-period 5 -o output/v5p5.xml &&
+python runnerTemp.py --nogui --veh-binomial 2 --ped-binomial 2 -o output/v2p2.xml &&
+python runnerTemp.py --nogui --veh-binomial 3 --ped-binomial 3 -o output/v3p3.xml &&
+python runnerTemp.py --nogui --veh-binomial 4 --ped-binomial 4 -o output/v4p4.xml &&
+python runnerTemp.py --nogui --veh-binomial 5 --ped-binomial 5 -o output/v5p5.xml &&
+python runnerTemp.py --nogui --veh-binomial 6 --ped-binomial 6 -o output/v6p6.xml &&
+python runnerTemp.py -d --nogui --veh-period 2 --ped-period 18 -o output/dv2p18.xml &&
+python runnerTemp.py -d --nogui --veh-period 3 --ped-period 4.5 -o output/dv3p4_5.xml &&
+python runnerTemp.py -d --nogui --veh-period 3.6 --ped-period 3.6 -o output/dv3_6p3_6.xml &&
+python runnerTemp.py -d --nogui --veh-period 4.5 --ped-period 3 -o output/dv4_5p3.xml &&
+python runnerTemp.py -d --nogui --veh-period 18 --ped-period 2 -o output/dv18p2.xml &&
+python runnerTemp.py -d --nogui -e 30 -o output/de30.xml &&
+python runnerTemp.py -d --nogui -e 60 -o output/de60.xml &&
+python runnerTemp.py -d --nogui -e 90 -o output/de90.xml &&
+python runnerTemp.py -d --nogui -e 120 -o output/de120.xml &&
+python runnerTemp.py -d --nogui -e 150 -o output/de150.xml &&
+python runnerTemp.py -d --nogui --veh-period 1 --ped-period 1 -o output/dv1p1.xml &&
+python runnerTemp.py -d --nogui --veh-period 2 --ped-period 2 -o output/dv2p2.xml &&
+python runnerTemp.py -d --nogui --veh-period 3 --ped-period 3 -o output/dv3p3.xml &&
+python runnerTemp.py -d --nogui --veh-period 4 --ped-period 4 -o output/dv4p4.xml &&
+python runnerTemp.py -d --nogui --veh-period 5 --ped-period 5 -o output/dv5p5.xml &&
+python runnerTemp.py -d --nogui --veh-binomial 2 --ped-binomial 2 -o output/dv2p2.xml &&
+python runnerTemp.py -d --nogui --veh-binomial 3 --ped-binomial 3 -o output/dv3p3.xml &&
+python runnerTemp.py -d --nogui --veh-binomial 4 --ped-binomial 4 -o output/dv4p4.xml &&
+python runnerTemp.py -d --nogui --veh-binomial 5 --ped-binomial 5 -o output/dv5p5.xml &&
+python runnerTemp.py -d --nogui --veh-binomial 6 --ped-binomial 6 -o output/dv6p6.xml
